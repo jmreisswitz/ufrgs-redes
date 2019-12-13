a894acb29201c67ffff7730fc755811013871a05
jmreisswitz <jmreisswitz@gmail.com>
2019-12-13 13:06:42 -0300

    add plotter

---
+++ plotter.py
@@ -0,0 +1,29 @@
+import argparse
+import time
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--csv', help='file name')
+parsed_args = parser.parse_args()
+
+
+def simple():
+	while True:
+		df = pd.read_csv(parsed_args.csv, sep=',')
+		df.plot()
+		plt.show()
+		time.sleep(3)
+
+
+def auto_update():
+	pass
+
+
+def main():
+	auto_update()
+
+
+if __name__ == '__main__':
+	main()
